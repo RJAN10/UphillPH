b6857e6b9b9f58daacb160f427e8d39fc508292f
RJNJQUN <mr.hustlemore@gmail.com>
2022-09-10 01:50:44 +0800

    New Push

---
+++ script.js
@@ -0,0 +1,18 @@
+// search button overlay
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
